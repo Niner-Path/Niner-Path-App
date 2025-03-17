dbbe0d87301101e78f987690524c291cc9977a6f
Smit Patel <smit.r.patel5@gmail.com>
2025-03-17 01:08:43 -0400

    Smit - TailwindCSS and Shadcn configuration

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,17 @@
-import "@/styles/globals.css";
+import "../src/styles/globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <SidebarProvider>
+      <div className="flex">
+        <AppSidebar />
+        <main className="flex-1 p-4">
+          <SidebarTrigger />
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </SidebarProvider>
+  );
 }
